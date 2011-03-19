059090ab7e7ec7d3038d5696d25ece5c7f13de6d
Scotty Allen <scotty@ooyala.com>
2011-03-19 15:29:47 -0700

    Got torch mode working.

--- iphone/StrobeCam/Classes/StrobeCamViewController.h
+++ iphone/StrobeCam/Classes/StrobeCamViewController.h
@@ -8,11 +8,22 @@
 
 #import <UIKit/UIKit.h>
 
+@class AVCaptureSession;
+@class AVCaptureDevice;
+@class AVCaptureDeviceInput;
+
 @interface StrobeCamViewController : UIViewController <UIImagePickerControllerDelegate, UINavigationControllerDelegate> {
 
 }
 
 - (IBAction)strobeButtonDown;
+
+@property(nonatomic, retain) AVCaptureSession *torchSession;
+@property(nonatomic, retain) AVCaptureDevice *device;
+@property(nonatomic, retain) AVCaptureDeviceInput *flashInput;
+@property(nonatomic, retain) NSTimer *flashTimer;
+@property(nonatomic, retain) AVCaptureStillImageOutput *output;
+
 
 @end
 
--- iphone/StrobeCam/Classes/StrobeCamViewController.m
+++ iphone/StrobeCam/Classes/StrobeCamViewController.m
@@ -6,65 +6,89 @@
 //  Copyright 2011 __MyCompanyName__. All rights reserved.
 //
 
+#import <AVFoundation/AVFoundation.h>
+
 #import "StrobeCamViewController.h"
+
+@interface StrobeCamViewController ()
+
+- (void)setupTorch;
+- (void)turnFlashOn;
+- (void)fireFlash;
+
+@end
 
 @implementation StrobeCamViewController
 
-
-
-/*
-// The designated initializer. Override to perform setup that is required before the view is loaded.
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
-*/
-
-/*
-// Implement loadView to create a view hierarchy programmatically, without using a nib.
-- (void)loadView {
-}
-*/
-
-
-/*
-// Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
-- (void)viewDidLoad {
-    [super viewDidLoad];
-}
-*/
-
-
-/*
-// Override to allow orientations other than the default portrait orientation.
-- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
-    // Return YES for supported orientations
-    return (interfaceOrientation == UIInterfaceOrientationPortrait);
-}
-*/
-
-- (void)didReceiveMemoryWarning {
-	// Releases the view if it doesn't have a superview.
-    [super didReceiveMemoryWarning];
-	
-	// Release any cached data, images, etc that aren't in use.
-}
-
-- (void)viewDidUnload {
-	// Release any retained subviews of the main view.
-	// e.g. self.myOutlet = nil;
-}
-
-
 - (void)dealloc {
+  [torchSession release];
+  [device release];
+  [flashInput release];
+  [flashTimer release];
   [super dealloc];
 }
 
 - (IBAction)strobeButtonDown {
   NSLog(@"button down");
+  [self fireFlash];
+}
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  [self setupTorch];
+}
+
+- (void) setupTorch {
+  self.device = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
+  if ([self.device hasTorch] && [self.device hasFlash]){
+    if (self.device.torchMode == AVCaptureTorchModeOff) {
+      NSLog(@"It's currently off.. turning on now.");
+      self.flashInput = [[AVCaptureDeviceInput deviceInputWithDevice:self.device error: nil] autorelease];
+      AVCaptureVideoDataOutput *output = [[[AVCaptureVideoDataOutput alloc] init] autorelease];
+      
+      AVCaptureSession *session = [[AVCaptureSession alloc] init];
+      
+      [session beginConfiguration];
+      [self.device lockForConfiguration:nil];
+      
+      [session addInput:flashInput];
+      [session addOutput:output];
+      
+      [self.device unlockForConfiguration];
+      
+      [session commitConfiguration];
+      [session startRunning];
+      
+      self.torchSession = session;
+    }
+    else {
+      NSLog(@"It's currently on.. turning off now.");
+      [torchSession stopRunning];
+    }
+  }
+}
+
+- (void)fireFlash {
+  [self turnFlashOn];
+  self.flashTimer = [NSTimer scheduledTimerWithTimeInterval:0.1
+                                                     target:self
+                                                   selector:@selector(turnFlashOff)
+                                                   userInfo:nil
+                                                    repeats:NO];
+}
+
+- (void)turnFlashOn {
+  [self.device lockForConfiguration:nil];
+//  self.device.flashMode = AVCaptureFlashModeOn;
+  self.device.torchMode = AVCaptureTorchModeOn;
+  [self.device unlockForConfiguration];
+}
+
+- (void)turnFlashOff {
+  [self.device lockForConfiguration:nil];
+//  self.device.flashMode = AVCaptureFlashModeOff;
+  self.device.torchMode = AVCaptureTorchModeOff;
+  [self.device unlockForConfiguration];
 }
 
 @end
